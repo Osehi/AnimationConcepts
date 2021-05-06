e6b37dc1d6b5a8a6ae4be07416c9741f11a194e7
Osehi <osehiaseehilen@gmail.com>
2021-05-06 14:48:22 +0100

    animation added

--- app/src/main/java/com/polish/createsplashscreen/MainActivity.kt
+++ app/src/main/java/com/polish/createsplashscreen/MainActivity.kt
@@ -2,8 +2,16 @@ package com.polish.createsplashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * declare variable
+     */
+    lateinit var topAnimation: Animation
+    lateinit var bottomAnimation: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
