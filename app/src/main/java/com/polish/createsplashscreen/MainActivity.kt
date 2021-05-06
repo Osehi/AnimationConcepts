0cc62eedcb31f2c6726d4c556abe4fe2d32528bf
Osehi <osehiaseehilen@gmail.com>
2021-05-06 15:10:50 +0100

    aninamtion implemented

--- app/src/main/java/com/polish/createsplashscreen/MainActivity.kt
+++ app/src/main/java/com/polish/createsplashscreen/MainActivity.kt
@@ -3,17 +3,44 @@ package com.polish.createsplashscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import com.polish.createsplashscreen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * binding reference object
+     */
+    lateinit var binding: ActivityMainBinding
 
     /**
      * declare variable
      */
     lateinit var topAnimation: Animation
     lateinit var bottomAnimation: Animation
+    lateinit var myCar:ImageView
+    lateinit var carName:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // initialize the animation
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        // initiaze views
+        myCar = binding.activityMainCarIv
+        carName = binding.activityMainCarNameTv
+
+        // set the animation to the views.
+        myCar.animation = topAnimation
+        carName.animation = bottomAnimation
+
+
     }
 }
